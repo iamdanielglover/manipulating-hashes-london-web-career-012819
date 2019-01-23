0ae28a1aca9b28567e0073787fbcf98b2d3f1689
github username <github email address>
2019-01-23 19:42:31 +0000

    Done.

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,3 +1,4 @@
+require 'pry'
 def second_challenge
   groceries = {
    dairy: ["milk", "yogurt", "cheese"],
@@ -7,7 +8,8 @@ def second_challenge
   }
 
   #code your solution here!
-
-  
+  new_arr = groceries.values
+  list = new_arr.join(", ").split(", ")
+  list
 
 end
